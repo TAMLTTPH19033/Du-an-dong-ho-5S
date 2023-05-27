2ead76e1bdf2af0a05508a1959691629a45d5bb7
Nguyen-TruongAn-2505 <99885211+YutaAn@users.noreply.github.com>
2023-05-27 23:32:20 +0700

    update
    new

--- Du-an-dong-ho-5S-10-5-2023_TaoKhungProject/src/main/java/com/example/ProjectDATN/Repository/HoaDonChiTietRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProjectDATN.Repository;
-
-import com.example.ProjectDATN.Entity.HoaDonChiTiet;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HoaDonChiTietRepository extends JpaRepository<HoaDonChiTiet,Integer> {
-}
--- Du-an-dong-ho-5S-10-5-2023_TaoKhungProject/src/main/java/com/example/ProjectDATN/Service/Impl/PinServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProjectDATN.Service.Impl;
-
-import com.example.ProjectDATN.Service.PinService;
-import com.example.ProjectDATN.Service.VatLieuService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class PinServiceImpl implements PinService {
-}
