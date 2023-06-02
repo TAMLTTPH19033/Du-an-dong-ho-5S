e172f00c43b347a8323b0ab25ae6c247783c8829
Nguyen-TruongAn-2505 <99885211+YutaAn@users.noreply.github.com>
2023-06-02 23:44:22 +0700

    update

---
+++ src/main/java/com/example/ProjectDATN/Controller/NhanVienRestController.java
@@ -0,0 +1,18 @@
+package com.example.ProjectDATN.Controller;
+
+import com.dongho5s.admin.Service.Impl.NhanVienService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NhanVienRestController {
+    @Autowired
+    private NhanVienService service;
+
+    @PostMapping("/users/check_email")
+    public String checkDuplicateEmail(@Param("id") Integer id ,@Param("email") String email){
+        return service.isEmailUnique(id , email) ? "OK" : "Duplicated";
+    }
+}
