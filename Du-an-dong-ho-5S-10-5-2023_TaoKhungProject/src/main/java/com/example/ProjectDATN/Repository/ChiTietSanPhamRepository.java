a386d61e66468b0c41b4950205401d1163e6d682
Nguyen-TruongAn-2505 <99885211+YutaAn@users.noreply.github.com>
2023-06-05 22:36:02 +0700

    quản lý thể loại

--- Du-an-dong-ho-5S-10-5-2023_TaoKhungProject/src/main/java/com/example/ProjectDATN/Repository/ChiTietSanPhamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ProjectDATN.Repository;
-
-import com.example.ProjectDATN.Entity.ChiTietSanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChiTietSanPhamRepository extends JpaRepository<ChiTietSanPham,Integer>{
-}
--- Du-an-dong-ho-5S-10-5-2023_TaoKhungProject/src/main/java/com/example/ProjectDATN/Service/Impl/KhuyenMaiServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.ProjectDATN.Service.Impl;
-
-import com.example.ProjectDATN.Service.KhuyenMaiService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class KhuyenMaiServiceImpl implements KhuyenMaiService {
-}
