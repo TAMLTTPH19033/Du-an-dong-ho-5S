24fbc1a7e43d4c33d7695cff28d1db30fce1ba90
tamltt02 <tamltt.2410@gmail.com>
2023-07-27 16:16:27 +0700

    tam change

--- src/main/resources/static/controller/index.js
+++ src/main/resources/static/controller/index.js
@@ -20,39 +20,37 @@ myApp.config(function ($routeProvider, $locationProvider,$httpProvider) {
       templateUrl: "page/blog.html",
     })
     .when("/sanpham", {
-      templateUrl: "page/sanpham.html",
+      templateUrl: "page/shop-gird.html",
       controller: "SanPhamController"
     })
-    .when("/info", {
-      templateUrl: "page/thongTinCaNhan.html",
+    .when("/account", {
+      templateUrl: "page/my-account.html",
       controller: "ThongTinCaNhanController"
     })
     .when("/cart", {
-      templateUrl: "page/giohang.html",
+      templateUrl: "page/cart.html",
       controller :"cartCtrl"
     })
     .when("/signup", {
-      templateUrl: "page/signup.html",
+      templateUrl: "page/checkout-registration.html",
       controller: "registerCtrl"
     })
     .when("/login", {
-      templateUrl: "page/login.html",
+      templateUrl: "page/checkout-signin.html",
       controller:"loginCtrl"
 
     })
-    .when("/changepass", {
-      templateUrl: "/page/doimatkhau.html",
-    })
-    .when("/thanhtoan", {
-      templateUrl: "page/thanhtoan.html",
+
+    .when("/checkout", {
+      templateUrl: "page/checkout.html",
       controller: "ThanhToanCtrl"
     })
     .when("/chitietsanpham/:idSp", {
-      templateUrl: "page/chitietsanpham.html",
+      templateUrl: "page/single-product.html",
       controller: "TrangChiTietSanPhamController"
     })
     .when("/suathongtincanhan", {
-      templateUrl: "page/suathongtincanhan.html",
+      templateUrl: "page/my-.html",
       controller: "ThongTinCaNhanController"
     })
     .otherwise({
