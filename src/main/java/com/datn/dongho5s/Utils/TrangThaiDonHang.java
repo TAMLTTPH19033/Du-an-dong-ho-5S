85b2ba4acc1bef829389465a167893be18ea949a
HoangDVPH18776 <101421633+HoangDVPH18776@users.noreply.github.com>
2023-08-04 23:30:44 +0700

    20230804 fixbug tim kiem san pham

---
+++ src/main/java/com/datn/dongho5s/Utils/TrangThaiDonHang.java
@@ -0,0 +1,12 @@
+package com.datn.dongho5s.Utils;
+
+public class TrangThaiDonHang {
+    public static Integer CHO_XAC_NHAN = 0;
+    public static Integer DANG_CHUAN_BI = 1;
+    public static Integer DANG_GIAO = 2;
+    public static Integer HOAN_THANH = 3;
+    public static Integer DA_HUY = 4;
+    public static Integer YEU_CAU_HOAN_TRA = 5;
+    public static Integer DA_HOAN_TRA = 6;
+
+}
