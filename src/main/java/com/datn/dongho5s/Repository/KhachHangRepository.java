341174a5627fbc3a2e34db1b5d18e0ff440ba4ba
baolq <baolqph18829@fpt.edu.vn>
2023-08-12 05:34:45 +0700

    Bao - Xem Thong Tin Khach Hang

--- src/main/java/com/datn/dongho5s/Repository/KhachHangRepository.java
+++ src/main/java/com/datn/dongho5s/Repository/KhachHangRepository.java
@@ -3,6 +3,8 @@ package com.datn.dongho5s.Repository;
 
 import com.datn.dongho5s.Entity.KhachHang;
 import com.datn.dongho5s.Entity.NhanVien;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,5 +18,9 @@ public interface KhachHangRepository extends JpaRepository<KhachHang,Integer> {
     @Query("SELECT kh FROM KhachHang kh WHERE kh.email = :email")
     public KhachHang getKhachHangByEmail(@Param("email") String email);
 
+    @Query("SELECT kh FROM KhachHang kh WHERE UPPER(CONCAT(kh.idKhachHang, ' ', kh.tenKhachHang, ' ', kh.email, '', kh.soDienThoai)) LIKE %?1%")
+    public Page<KhachHang> findAll(String keyword, Pageable pageable);
+
+    public KhachHang findByTenKhachHang(String ten);
 
 }
--- src/main/java/com/datn/dongho5s/Service/KhachHangService.java
+++ src/main/java/com/datn/dongho5s/Service/KhachHangService.java
@@ -3,9 +3,13 @@ package com.datn.dongho5s.Service;
 import com.datn.dongho5s.Entity.KhachHang;
 import com.datn.dongho5s.Response.ThongTinCaNhanResponse;
 import com.datn.dongho5s.Response.ThongTinToCheckoutResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 public interface KhachHangService {
+    public static final int CUSTOMERS_PER_PAGE = 4;
     public KhachHang findKhachHangById(Integer id);
     public KhachHang updateThongTinCaNhan(Integer idKhachhang,ThongTinCaNhanResponse thongTinCaNhanResponse);
     public ThongTinCaNhanResponse getThongTinCaNhanById(Integer id);
@@ -13,4 +17,9 @@ public interface KhachHangService {
 
     public void saveKhachHang(KhachHang khachHang);
 
+    List<KhachHang> getAllKhachHang();
+
+    List<KhachHang> getAllPaginationVatLieu();
+
+    Page<KhachHang> listByPage(int pageNumber, String sortField, String sortDir, String keyword);
 }
